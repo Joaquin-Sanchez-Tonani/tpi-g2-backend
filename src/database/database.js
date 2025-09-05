6a46bb015012bd9779f2962ab6d12e3a2f1af863
Joaquin-Sanchez-Tonani <joaquinsancheztonani15@gmail.com>
2025-09-05 00:58:45 -0300

    Added db connection

--- src/database/database.js
+++ src/database/database.js
@@ -1,5 +1,8 @@
 import { Sequelize } from "sequelize";
 
+// Conexión local de Joaquin
+
+/*
 export const sequelize = new Sequelize('medical_clinic', 'postgres', 'JN15Mosk', {
   host: 'localhost',
   port: 4000,   
@@ -8,3 +11,16 @@ export const sequelize = new Sequelize('medical_clinic', 'postgres', 'JN15Mosk',
     ssl: false
   }
 });
+*/
+
+// Servidor para desarrollo
+
+export const sequelize = new Sequelize('tgi-g2', 'consultor', 'SilkSong2025@@', {
+  host: 'localhost',
+  port: 4001,   
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: false
+  }
+});
+
