6e6405c5b34b5b128cdb49e63a998f6fd81a73d8
Augusto Magi <augustoagi12345@gmail.com>
2025-10-04 14:23:56 -0300

    Git specialities added at home

---
+++ src/utils/appointment.data.js
@@ -0,0 +1,12 @@
+export const appointment = [
+     { date: "2025-10-10", time_id: 1, patient_id: 2, specialist_id: 5 },
+    { date: "2025-10-11", time_id: 2, patient_id: 3, specialist_id: 6 },
+    { date: "2025-10-12", time_id: 3, patient_id: 4, specialist_id: 7 },
+    {  date: "2025-10-13", time_id: 4, patient_id: 5, specialist_id: 8 },
+    { date: "2025-10-14", time_id: 5, patient_id: 6, specialist_id: 9 },
+    {  date: "2025-10-15", time_id: 6, patient_id: 7, specialist_id: 1 },
+    {  date: "2025-10-16", time_id: 7, patient_id: 8, specialist_id: 3 },
+    { date: "2025-10-17", time_id: 8, patient_id: 9, specialist_id: 2 },
+    {date: "2025-10-18", time_id: 9, patient_id: 10, specialist_id: 7 },
+    {date: "2025-10-19", time_id: 10, patient_id: 11, specialist_id: 6 }
+]
